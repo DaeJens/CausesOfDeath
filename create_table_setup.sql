445bc70550ef9cf80c04f5f9a2269adff7520ab8
Daelin Jensen <86317765+DaeJens@users.noreply.github.com>
2025-06-17 22:01:32 -0400

    create_tables_setup

---
+++ create_table_setup.sql
@@ -0,0 +1,310 @@
+-- Global Burden of Disease Collaborative Network.
+-- Global Burden of Disease Study 2021 (GBD 2021) Results.
+-- Seattle, United States: Institute for Health Metrics and Evaluation (IHME), 2022.
+-- Available from https://vizhub.healthdata.org/gbd-results/.
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_01 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year smallint,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_02 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_03 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_04 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_05 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_06 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_07 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_08 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_09 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_10 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_11 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_12 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_13 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_14 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_15 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_16 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
+
+CREATE TABLE IF NOT EXISTS `globalburdenofdisease`.table_17 (
+	measure_id integer,
+    measure_name character varying(255),
+    location_id integer,
+    location_name character varying(255),
+    sex_id integer,
+    sex_name character varying(255),
+    age_id integer,
+    age_name character varying(255),
+    cause_id integer,
+    cause_name character varying(255),
+    metric_id integer,
+    year year,
+    val float,
+    upper float,
+    lower float
+);
